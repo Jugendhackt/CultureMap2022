f40774085badcb1a7a64f5abb3d838297a047e57
Dlurak <dimitrij.m@outlook.de>
2022-06-18 17:10:41 +0200

    Added a function to add a marker on the map

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.477, 0], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+function addPoint(coordinates) {
+    var marker = L.marker(coordinates);
+    marker.addTo(map);
+}
+
+addPoint([51.477, 0]);
