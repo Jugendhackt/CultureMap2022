9395409bbc90a9aa8762c5d7105db81dbd80ad91
Dlurak <dimitrij.m@outlook.de>
2022-06-18 18:27:45 +0200

    Added points and shapes for attractions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.477, 0], 13);
+var map = L.map('map').setView([50.1174, 8.684], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -8,6 +8,50 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function addPoint(coordinates) {
     var marker = L.marker(coordinates);
     marker.addTo(map);
+    return marker;
 }
 
-addPoint([51.477, 0]);
+function buildOverpassApiUrl(map, overpassQuery) {
+    var bounds = map.getBounds().getSouth() + ',' + map.getBounds().getWest() + ',' + map.getBounds().getNorth() + ',' + map.getBounds().getEast();
+    var nodeQuery = 'node[' + overpassQuery + '](' + bounds + ');'; // points
+    var wayQuery = 'way[' + overpassQuery + '](' + bounds + ');'; // line
+    var relationQuery = 'relation[' + overpassQuery + '](' + bounds + ');'; //group of objects
+    var query = '?data=[out:json][timeout:15];(' + nodeQuery + wayQuery + relationQuery + ');out body geom;';
+    var baseUrl = 'http://overpass-api.de/api/interpreter';
+    var resultUrl = baseUrl + query;
+    return resultUrl;
+}
+
+function createLayer() {
+    var filter = 'tourism=attraction'
+    var overpassApiUrl = buildOverpassApiUrl(map, filter);
+    
+    $.get(overpassApiUrl, function (osmDataAsJson) {
+        var resultAsGeojson = osmtogeojson(osmDataAsJson);
+        var resultLayer = L.geoJson(resultAsGeojson, {
+        style: function (feature) {
+            return {color: "#ff0000"};
+        },
+        /* filter: function (feature, layer) {
+            var isPolygon = (feature.geometry) && (feature.geometry.type !== undefined) && (feature.geometry.type === "Polygon");
+            if (isPolygon) {
+            feature.geometry.type = "Point";
+            var polygonCenter = L.latLngBounds(feature.geometry.coordinates[0]).getCenter();
+            feature.geometry.coordinates = [ polygonCenter.lat, polygonCenter.lng ];
+            }
+            return true;
+        }, */
+        /* onEachFeature: function (feature, layer) {
+            var popupContent = "";
+            popupContent = popupContent + "<dt>@id</dt><dd>" + feature.properties.type + "/" + feature.properties.id + "</dd>";
+            var keys = Object.keys(feature.properties.tags);
+            keys.forEach(function (key) {
+            popupContent = popupContent + "<dt>" + key + "</dt><dd>" + feature.properties.tags[key] + "</dd>";
+            });
+            popupContent = popupContent + "</dl>"
+            layer.bindPopup(popupContent);
+        } */
+        }).addTo(map);
+    });
+}
+createLayer()
