f28bbf499521b18f4d4480a9290e62a725db34b6
Computerkrack <david-hoos@web.de>
2022-06-19 11:15:02 +0200

    Datum und Titel werden nebeneinander gezeigt

--- event_downloader(sankt-peter).py
+++ event_downloader(sankt-peter).py
@@ -1,18 +1,21 @@
-from re import U
 import requests 
 from bs4 import BeautifulSoup
 
 r = requests.get('https://sanktpeter.com/angebote/')
 soup = BeautifulSoup(r.text, 'html.parser')
 
-'''for event in soup.find_all(class_="events_desc"): 
+for event in soup.find_all(class_="events_desc"): 
     print(event.string)
 
     date = event.find (class_="date-line")
-    print(date.text)'''
     
-ueberschriften = soup.find_all("h3")
-#print(ueberschrift.text)
+    
+    title = event.find ("h3")
+    print(date.text, title.string)
+
+#test
+'''ueberschriften = soup.find_all("h3")
+print(ueberschrift.text)
 
 for i in ueberschriften:
-    print (ueberschriften)
+    print (i.string)'''
